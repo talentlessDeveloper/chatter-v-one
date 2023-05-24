598e8f41b7f9f5cf829b896d4b7d73b51506b4f6
talentlessDeveloper <kareemope52@gmail.com>
2023-05-24 11:56:20 +0100

    added text to homepage

--- chatter/src/pages/home/Home.tsx
+++ chatter/src/pages/home/Home.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className='text-red-500 text-3xl text-center'>
+      Welcome to CHATTER!!
+    </div>
+  );
 };
 
 export default Home;
