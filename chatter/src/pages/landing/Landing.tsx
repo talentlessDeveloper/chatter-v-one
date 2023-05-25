249a3ce43f02f04d4b9416a8a83197acfbba29f6
talentlessDeveloper <kareemope52@gmail.com>
2023-05-25 22:12:10 +0100

    landing page

---
+++ chatter/src/pages/landing/Landing.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TopNav from "../../components/shared/TopNav";
+import Hero from "../../components/page/landing/Hero";
+
+const Landing = () => {
+  return (
+    <div>
+      <TopNav />
+      <Hero />
+    </div>
+  );
+};
+
+export default Landing;
